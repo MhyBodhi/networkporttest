c3d045bbf2ff880c6e9ff07b04d00e714807402f
mhybodhi <2436810573@qq.com>
2020-08-20 10:03:56 +0800

    ok

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 #Installation dependent environment
 sudo bash install.bash
 
-#netperf test
+#netperf 60 seconds for each test
 sudo bash netperf/netperf.sh 192.168.1.100 60
-#iperf3 test
+#iperf3 The test time is 200 seconds, each item runs for 60 seconds, and the Ping Test -c parameter is 10
 sudo bash iperf3/iperf3.sh 192.168.1.100 200 60 10
 #nc test
- #server
+ #server listen to 5000 port,redis server ip 192.168.1.206
 sudo bash ncserver.sh 5000 192.168.1.206
- #client
+ #client connect server ip 192.168.1.217,port 5000,test01 is the transfer file,redis server 192.168.1.206
 sudo bash ncclient.sh 192.168.1.217 5000 test01.sh 192.168.1.206
