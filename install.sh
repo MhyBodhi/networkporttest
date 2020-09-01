3943ba5a05aec621063bd9bc7d88db6442621069
mhybodhi <2436810573@qq.com>
2020-09-01 11:09:53 +0800

    ok

--- install.sh
+++ install.sh
@@ -5,15 +5,14 @@ deb http://archive.kylinos.cn/kylin/KYLIN-ALL 4.0.2sp2-desktop main restricted u
 MHY
 test -e ~/.pip/pip.conf
 if [ $? -ne 0 ];then
-	mkdir ~/.pip
-	cat > ~/.pip/pip.conf <<-MHY
-		[global]
-		timeout = 6000
-		index-url = https://pypi.tuna.tsinghua.edu.cn/simple
-		trusted-host = pypi.tuna.tsinghua.edu.cn	
-	MHY
+mkdir ~/.pip
+cat > ~/.pip/pip.conf <<-MHY
+[global]
+timeout = 6000
+index-url = https://pypi.tuna.tsinghua.edu.cn/simple
+trusted-host = pypi.tuna.tsinghua.edu.cn	
+MHY
 fi
-
 sudo apt -y update
 sudo apt -y install python3-pip
 sudo python3 -m pip install --upgrade pip
